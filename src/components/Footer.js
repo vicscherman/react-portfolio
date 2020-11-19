32419f744e3fee59960fdf9fb330d1626019f3db
Vic Scherman <vicscherman@gmail.com>
2020-11-19 10:02:10 -0500

    ./assets/resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import resume from "assets/resume.pdf"
+import resume from "./assets/resume.pdf"
 
 export default class Footer extends Component{
     render(){
